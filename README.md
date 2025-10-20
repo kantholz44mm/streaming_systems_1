4c3ac4f2a65174cc42fff0db97e0b81165acce64
Matthias Raba <matthias@raba.live>
2025-10-20 12:09:15 +0200

    added simple 2D vector class

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Streaming Systems,
 
 die erste.
+
+Bauen mit (Annahme: du bist im Ordner "ss"):
+mvn package
+
+Ausführen mit:
+mvn exec:java -Dexec.mainClass="com.krassedudes.ss.App" 
---
+++ ss/src/main/java/com/krassedudes/ss/Vector2D.java
@@ -0,0 +1,33 @@
+package com.krassedudes.ss;
+
+public class Vector2D
+{
+    public double x = 0;
+    public double y = 0;
+
+    public Vector2D(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2D()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public double length()
+    {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Vector2D delta(Vector2D other)
+    {
+        return new Vector2D(this.x - other.x, this.y - other.y);
+    }
+
+    public double angle() {
+        return Math.atan2(this.y, this.x);
+    }
+}
